6477cf60969d1d47eb2092096a2c12b99729fa4f
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-14 22:25:17 -0400

    Created EventList, EventListItem, and EventListAttendee components

---
+++ src/features/event/EventList/EventList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EventList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>EventList</h1>
+      </div>
+    );
+  }
+}
---
+++ src/features/event/EventList/EventListItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EventListItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>EventListItem</h1>
+      </div>
+    );
+  }
+}
