1ff515a3c8a28cc896103425580fb23da1737038
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-15 14:29:00 -0400

    Connected EventDetailedPage to redux store and displayed events based off of url params id

--- src/features/event/EventDetailed/EventDetailedPage.js
+++ src/features/event/EventDetailed/EventDetailedPage.js
@@ -4,22 +4,37 @@ import EventDetailedHeader from "./EventDetailedHeader";
 import EventDetailedInfo from "./EventDetailedInfo";
 import EventDetailedChat from "./EventDetailedChat";
 import EventDetailedSidebar from "./EventDetailedSidebar";
+import { connect } from "react-redux";
 
-const EventDetailedPage = () => {
+const EventDetailedPage = ({ event }) => {
   return (
     <div>
       <Grid>
         <Grid.Column width={10}>
-          <EventDetailedHeader />
-          <EventDetailedInfo />
+          <EventDetailedHeader event={event} />
+          <EventDetailedInfo event={event} />
           <EventDetailedChat />
         </Grid.Column>
         <Grid.Column width={6}>
-          <EventDetailedSidebar />
+          <EventDetailedSidebar attendees={event.attendees} />
         </Grid.Column>
       </Grid>
     </div>
   );
 };
 
-export default EventDetailedPage;
+const mapState = (state, ownProps) => {
+  const eventId = ownProps.match.params.id;
+
+  let event = {};
+
+  if (eventId && state.events.length > 0) {
+    event = state.events.filter(event => event.id === eventId)[0];
+  }
+
+  return {
+    event
+  };
+};
+
+export default connect(mapState)(EventDetailedPage);
