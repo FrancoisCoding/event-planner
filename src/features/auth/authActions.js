cc6b0fea209e09c375d4c0d3eb0bd77e1eb58e92
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-19 00:46:45 -0400

    Connected register reducer to register form

--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -25,3 +25,29 @@ export const logout = () => {
     type: SIGN_OUT_USER
   };
 };
+
+export const registerUser = user => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  try {
+    let createdUser = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(user.email, user.password);
+    console.log(createdUser);
+    await createdUser.user.updateProfile({
+      displayName: user.displayName
+    });
+    let newUser = {
+      displayName: user.displayName,
+      createdAt: firestore.FieldValue.serverTimestamp()
+    };
+    await firestore.set(`users/${createdUser.user.uid}`, { ...newUser });
+    dispatch(closeModal());
+  } catch (error) {
+    console.log(error);
+  }
+};
