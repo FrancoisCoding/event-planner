86944e5f716c3214ab3a7347f04e951ca3b73e5a
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-19 11:00:41 -0400

    Added facebook and google button to login and register form

--- src/features/auth/Register/RegisterForm.js
+++ src/features/auth/Register/RegisterForm.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import { combineValidators, isRequired } from "revalidate";
-import { Form, Segment, Button, Label } from "semantic-ui-react";
+import { Form, Segment, Button, Label, Divider } from "semantic-ui-react";
 import { Field, reduxForm } from "redux-form";
 import TextInput from "../../../app/common/form/TextInput";
 import { registerUser } from "../authActions";
+import SocialLogin from "../SocailLogin/SocialLogin";
 
 const actions = {
   registerUser
@@ -62,6 +63,8 @@ const RegisterForm = ({
           >
             Register
           </Button>
+          <Divider horizontal>Or</Divider>
+          <SocialLogin />
         </Segment>
       </Form>
     </div>
